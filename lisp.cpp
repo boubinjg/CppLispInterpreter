14380f8164dac75c900a3b887d73b94eb56334ef
boubinjg <boubinjg@gmail.com>
2018-02-24 17:55:18 -0500

    Reads normal interpretation and list interpretation into internal rep. error checking/reusing symbolIDs soon

--- lisp.cpp
+++ lisp.cpp
@@ -16,11 +16,12 @@ struct SExp{
 	SExp* left; SExp* right; //if type 3;
 };
 
+SExp* convertList(std::vector<token> exp);
+SExp* convertToInternalRep(std::vector<token> exp);
+
 //read std::cin until $$ is encountered
 //then split by $ into a vector
 void print(SExp* e){
-	//std::cout<<"in print"<<std::endl;
-	//std::cout<<e->val<<std::endl;
 	if(e->type == 3){
 		std::cout<<"(";
 		print(e->left);
@@ -69,77 +70,8 @@ std::vector<std::string> readInput(){
 	
 	return expAsStr;
 }
-std::vector<token> tokenize(std::string s){
-	std::vector<token> tokenizedStr;
-	for(auto it = s.begin(); it!=s.end(); ++it){
-		if(*it == '('){
-			token t{.tokenType = "lParen", .tokenText="("};
-			tokenizedStr.push_back(t);
-		}
-		else if(*it == ')'){
-			token t{.tokenType = "rParen", .tokenText=")"};
-			tokenizedStr.push_back(t);
-		}
-		else if(isspace(*it)){
-			token t{.tokenType = "whitespace", .tokenText=" "};
-			tokenizedStr.push_back(t);
-		}
-		else if(*it == '.'){
-			token t{.tokenType = "dot", .tokenText="."};
-			tokenizedStr.push_back(t);	
-		}
-		else if(isdigit(*it)){
-			auto beg = it;
-			for(auto itt = it; it != s.end() && isdigit(*itt); itt++)
-				it = itt;
-			std::string s(beg, it+1);
-			token t{.tokenType = "int", .tokenText=s};
-			tokenizedStr.push_back(t);
-		}
-		else if(isalpha(*it)){
-			auto beg = it;
-			//allows for alphanumeric symbolic IDs as long as they start with a letter
-			for(auto itt = it; it != s.end() && isalnum(*itt); itt++)
-				it = itt;
-			std::string s(beg, it+1);
-			token t{.tokenType = "symbolId", .tokenText=s};
-			tokenizedStr.push_back(t);
-		}
-		else{
-			std::cout<<"Unrecognized Token"<<std::endl;
-		}
-		
-	}
-	return tokenizedStr; 
-}
-size_t findMidDot(std::vector<token> exp){
-	std::cout<<"inMidDot"<<std::endl;
-	int depth = 0;
-	for(auto it = exp.begin(); it!=exp.end(); ++it){
-		if(it->tokenType == "lParen")
-			++depth;
-		else if(it->tokenType == "rParen")
-			--depth;
-		else if(it->tokenType == "dot" && depth==0){
-			std::cout<<it-exp.begin()<<std::endl;
-			return (it-exp.begin())+1;
-		}
-	}
-	return exp.begin()-exp.end();
-}
-std::vector<token> findCarandCdr(std::vector<token>& exp){
-	exp.erase(exp.begin());
-	exp.erase(exp.end()-1);
-
-	std::vector<token> car, cdr;
-
-	if(exp[0].tokenType == "int"){
-		car.push_back(exp[0]);
-		exp.erase(exp.begin());		
-	} else if(exp[0].tokenType == "symbolId"){
-
-	}
-
+bool isAtom(token t){
+	return (t.tokenType == "int" || t.tokenType == "symbolId");
 }
 void trim(std::vector<token>& exp){
 	auto wsfront = exp.begin()-1, wsend = exp.end();
@@ -164,48 +96,184 @@ void trim(std::vector<token>& exp){
 	}
 	
 }
+std::vector<token> tokenize(std::string s){
+	std::vector<token> tokenizedStr;
+	for(auto it = s.begin(); it!=s.end(); ++it){
+		if(*it == '('){
+			token t{.tokenType = "lParen", .tokenText="("};
+			tokenizedStr.push_back(t);
+		} else if(*it == ')'){
+			token t{.tokenType = "rParen", .tokenText=")"};
+			tokenizedStr.push_back(t);
+		} else if(isspace(*it)){
+			token t{.tokenType = "whitespace", .tokenText=" "};
+			tokenizedStr.push_back(t);
+			auto beg = it;
+			for(auto itt = it; it != s.end() && isspace(*itt); itt++)
+				it = itt;
+		} else if(*it == '.'){
+			token t{.tokenType = "dot", .tokenText="."};
+			tokenizedStr.push_back(t);	
+		} else if(isdigit(*it)){
+			auto beg = it;
+			for(auto itt = it; it != s.end() && isdigit(*itt); itt++)
+				it = itt;
+			std::string s(beg, it+1);
+			token t{.tokenType = "int", .tokenText=s};
+			tokenizedStr.push_back(t);
+		} else if(isalpha(*it)){
+			auto beg = it;
+			//allows for alphanumeric symbolic IDs as long as they start with a letter
+			for(auto itt = it; it != s.end() && isalnum(*itt); itt++)
+				it = itt;
+			std::string s(beg, it+1);
+			token t{.tokenType = "symbolId", .tokenText=s};
+			tokenizedStr.push_back(t);
+		} else{
+			std::cout<<"Unrecognized Token"<<std::endl;
+		}
+	}
+	return tokenizedStr; 
+}
+std::vector<token> findCarAndCdr(std::vector<token>& exp){
+	exp.erase(exp.begin());
+	exp.erase(exp.end()-1);
+
+	std::vector<token> car, cdr;
+	trim(exp);
+	
+	if(exp[0].tokenType == "rParen" || exp[0].tokenType == "dot") {
+		std::cout<<"Invalid Token"<<std::endl;
+	} else if(exp[0].tokenType == "int" || exp[0].tokenType == "symbolId"){
+		car.push_back(exp[0]);
+		exp.erase(exp.begin());		
+	} else if(exp[0].tokenType == "lParen"){
+		auto end = exp.begin() + 1;
+		int level = 0;
+		for(auto it = exp.begin(); it != exp.end(); it++){
+			if(it->tokenType == "rParen") {
+				++level;
+			} else if(it->tokenType == "lParen") {
+				--level;
+			}
+			if(level == 0){
+				end = it;
+				break;
+			}
+		}
+		car = std::vector<token>(exp.begin(), end+1);
+		exp.erase(exp.begin(), end+1);
+	}
+	std::cout<<"CAR"<<std::endl;
+	print(car);
+	
+	if(exp.size() == 0){
+		token t{.tokenType = "symbolId", .tokenText="NIL"};
+		cdr.push_back(t);
+	} else if(exp[0].tokenType == "whitespace"){
+		if(exp[1].tokenType == "dot") {
+			cdr = std::vector<token>(exp.begin()+2, exp.end());
+			trim(cdr);
+		} else
+			cdr = std::vector<token>(exp.begin(), exp.end());
+	} else if(exp[0].tokenType == "dot"){
+		cdr = std::vector<token>(exp.begin()+1, exp.end());
+		trim(cdr);
+	} else
+		std::cout<<"error1"<<std::endl;
+	std::cout<<"CDR"<<std::endl;
+	print(cdr);
+
+	exp = cdr; //great practice, thumbs up.
+	return car;
+}
+SExp* convertList(std::vector<token> exp){
+	std::cout<<"ctl"<<std::endl;
+	trim(exp);
+	SExp *e = new SExp;
+	e->type = 3;
+	if(exp.size() > 1){
+		if(isAtom(exp[0])) {		
+			e->left = convertToInternalRep(std::vector<token>(exp.begin(), exp.begin()+1));
+			e->right = convertList(std::vector<token>(exp.begin()+1, exp.end()));
+		} else if(exp[0].tokenType == "lParen"){
+			auto rp = exp.begin();
+			int level = 0;
+			for(auto it = exp.begin(); it != exp.end(); it++){
+				if(it->tokenType == "rParen"){
+					++level;
+				} else if(it->tokenType == "lParen"){
+					--level;
+				}
+				if(level == 0){
+					rp = it;
+					break;
+				}	
+			}
+			if(rp == exp.begin()){
+				std::cout<<"error2"<<std::endl;
+			} else if(rp+1 == exp.end()){
+				SExp *nil = new SExp;
+				nil->type = 2;
+				nil->name = "NIL";
+				e->left = convertToInternalRep(std::vector<token>(exp.begin(), rp+1));
+				e->right = nil;
+			} else {
+				e->left = convertToInternalRep(std::vector<token>(exp.begin(), rp+1));
+				e->right = convertList(std::vector<token>(rp+1, exp.end()));
+			}	
+		} else {	
+			std::cout<<"error3"<<std::endl;
+			print(exp);
+		}
+	} else if(isAtom(exp[0])) {
+		SExp *nil = new SExp;
+		nil->type = 2;
+		nil->name = "NIL";
+		
+		e->left = convertToInternalRep(std::vector<token>(exp.begin(), exp.begin()+1));
+		e->right = nil;
+	}
+	std::cout<<"leave ctl"<<std::endl;
+	return e;
+}
 SExp* convertToInternalRep(std::vector<token> exp){
 	SExp *e = new SExp;
-	std::cout<<"in"<<std::endl;
 	trim(exp);
 	if(exp.size() == 1){
 		if(exp[0].tokenType == "int"){
 			e->type = 1;
 			e->val = std::stoi(exp[0].tokenText);
-			//std::cout<<"e1"<<std::endl;
 			return e;
-		}
-		else if(exp[0].tokenType == "symbolId"){
+		} else if(exp[0].tokenType == "symbolId"){
 			e->type = 2;
 			e->name = exp[0].tokenText;
-			//std::cout<<"e2"<<std::endl;
 			return e;
 		}
-	}
-	else if(exp[0].tokenType == "lParen" && exp[exp.size()-1].tokenType == "rParen"){
-		//auto nextDot = std::find_if(exp.begin()+1, exp.end()-1, [](token t){return t.tokenType == "dot";});
-		auto nextDot = findMidDot(std::vector<token>(exp.begin()+1, exp.end()-1));
+	} else if(exp[0].tokenType == "lParen" && exp[exp.size()-1].tokenType == "rParen"){
+		//find null list
+		if(exp.size() == 2 || (exp.size() == 3 && exp[1].tokenType == "whitespace")){ 
+			e->type = 2;
+			e->name = "NIL";
+		} else {
 
-		std::vector<token> car = findCarAndCdr(exp);		
+			std::vector<token> car = findCarAndCdr(exp);		
+			std::vector<token> cdr = exp;
 
-		e->type = 3;
-		std::cout<<(exp.begin()+1)->tokenType<<std::endl;
-		std::cout<<(exp.begin()+nextDot-1)->tokenType<<std::endl;
-
-		e->left = convertToInternalRep(std::vector<token>(exp.begin()+1, exp.begin()+nextDot));
-		std::cout<<"next2"<<std::endl;
-		e->right = convertToInternalRep(std::vector<token>(exp.begin()+nextDot+1, exp.end()-1));
-		//std::cout<<"e3"<<std::endl;
+			e->type = 3;
+			e->left = convertToInternalRep(car);
+			if(cdr[0].tokenType == "whitespace"){
+				e->right = convertList(cdr);
+			} else {
+				e->right = convertToInternalRep(cdr);
+			}
+		}
 		return e;
 	} else {
-		//std::cout<<exp[0].tokenType<<" "<<exp[0].tokenText<<std::endl;
-		//std::cout<<exp[exp.size()-1].tokenType<<" "<<exp[exp.size()-1].tokenText<<std::endl;
-		std::cout<<"Not an S Expression"<<std::endl;
+		std::cout<<"error 4"<<std::endl;
 		print(exp);
-		//std::cout<<"e4"<<std::endl;
 		return e;
 	}
-	//std::cout<<"e5"<<std::endl;
 	return e;
 }
 int main(){
@@ -217,7 +285,7 @@ int main(){
 	
 	for(auto exp : tokenizedExprs){
 		SExp* e = convertToInternalRep(exp);
+		findCarAndCdr(exp);
 		print(e);
-		//std::cout<<e->left->val<<std::endl;
 	}
 }
